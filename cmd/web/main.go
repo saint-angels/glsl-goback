fccf2d8e1df92b05b642a4310bc4eed78056a0f2
michael <mikeprokofyev@gmail.com>
2021-03-22 15:43:02 +0300

    init.sql added

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	"flag"
+	"database/sql"
 	"log"
 	"net/http"
 	"os"
 	"time"
+
+	"saint-angels/glsl-goback/pkg/models"
+	"saint-angels/glsl-goback/pkg/models/mysql"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type contextKey string
@@ -15,21 +20,36 @@ const contextKeyIsAuthenticated = contextKey("isAuthenticated")
 type application struct {
 	errorLog *log.Logger
 	infoLog *log.Logger
+	artworks interface {
+		Insert() (int, error)
+		GetOldestUnrendered() (*models.Artwork, error)
+	}
 }
 
 func main() {
-
 	addr := flag.String("addr", ":4000", "HTTP network address")
+	dsn := flag.String("dsn", "web:pass@/shaderbox?parseTime=true", "MySQL data source name")
+
 	flag.Parse()
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate | log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate | log.Ltime | log.Lshortfile)
 
 
+	//Get pool of database connections
+    db, err := sql.Open("mysql", *dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+    if err = db.Ping(); err != nil {
+		errorLog.Fatal(err)
+    }
+	defer db.Close()
 
 	app := &application {
 		infoLog: infoLog,
 		errorLog: errorLog,
+		artworks: &mysql.ArtworkModel{DB: db},
 	}
 
 	srv := &http.Server {
@@ -43,6 +63,6 @@ func main() {
 	}
 
 	infoLog.Printf("starting server on %s", *addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	errorLog.Fatal(err)
 }
---
+++ pkg/models/mysql/init.sql
@@ -0,0 +1,22 @@
+-- sudo mysql < ./pkg/models/mysql/init.sql
+drop database IF EXISTS shaderbox;
+
+CREATE DATABASE shaderbox CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE shaderbox;
+
+CREATE TABLE artworks (
+    id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    created DATETIME NOT NULL
+);
+-- Add an index on the created column.
+CREATE INDEX idx_artworks_created ON artworks(created);
+
+
+DROP USER IF EXISTS 'web'@'localhost';
+FLUSH PRIVILEGES;
+CREATE USER 'web'@'localhost';
+GRANT SELECT, INSERT, UPDATE ON shaderbox.* TO 'web'@'localhost';
+ALTER USER 'web'@'localhost' IDENTIFIED BY 'pass';
+
+SHOW TABLES;
+SELECT user from mysql.user;
