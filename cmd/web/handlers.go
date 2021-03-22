91ec4bec70bebefb4e8d9b86130a54ab20db23d4
michael <mikeprokofyev@gmail.com>
2021-03-22 22:21:20 +0300

    render workers in progress

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"net/http"
-	"os/exec"
     "saint-angels/glsl-goback/pkg/renderer"
+    "fmt"
 )
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
@@ -11,48 +11,18 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) generate(w http.ResponseWriter, r *http.Request) {
-    err := renderer.Render()
+    id, err := app.artworks.Insert()
     if err != nil {
-       app.serverError(w, err)
-       return
+        app.serverError(w, err)
+        return
     }
-    w.Write([]byte("rendering finished!"))
-}
 
-func (app *application) generate2(w http.ResponseWriter, r *http.Request) {
-    // path, err := exec.LookPath("shady")
-    // if err != nil {
-    //    app.serverError(w, errors.New("shady not found"))
-    //    return
-    // }
-    // app.infoLog.Printf("shady path: %s",path)
-    //
-    shaderName := "seascape"
-    shaderPath := "./shaders/" + shaderName + ".glsl"
-    encoding := "x11"
-    resolution := "1080x1080"
-    cmdShady := exec.Command("/home/teeth/go/bin/shady", "-i", shaderPath, "-ofmt", encoding, "-g",resolution, "-framerate", "30")
-    stdoutStderr, err := cmdShady.CombinedOutput()
-
-    app.infoLog.Printf("shady output: %s",stdoutStderr)
+    err = renderer.Render(id)
     if err != nil {
        app.serverError(w, err)
        return
     }
 
-    // videoDuration := "10"
-    // outputPath := "./renders/" + shaderName
-    // cmdffmpeg := exec.Command(
-    //     "/usr/bin/ffmpeg",
-    //     "-f", "rawvideo",
-    //     "-pixel_format", "rgb24",
-    //     "-video_size", "1080x1080",
-    //     "-framerate", "30",
-    //     "-t", videoDuration,
-    //     "-i", "-", outputPath,
-    //     "-y")
-
-	// app.infoLog.Printf("%s - %s %s %s", r.RemoteAddr, r.Proto, r.Method, r.URL.RequestURI())
-
-    w.Write([]byte("rendering finished!"))
+    reply := fmt.Sprintf("art id:%d", id)
+    w.Write([]byte(reply))
 }
--- pkg/renderer/renderer.go
+++ pkg/renderer/renderer.go
@@ -4,9 +4,10 @@ import (
 	"fmt"
 	"os/exec"
 	"errors"
+	"saint-angels/glsl-goback/pkg/models"
 )
 
-func Render() (error) {
+func Render(artId int) (error) {
     pathShady, err := exec.LookPath("shady")
     if err != nil {
        return errors.New("renderer: shady not found")
@@ -24,7 +25,7 @@ func Render() (error) {
     ffmpegPath := pathffmpeg
     ffmpegEncoding := "rgb24"
     videoDuration := "10"
-    outputPath := "./renders/" + shaderName + ".avi"
+    outputPath := fmt.Sprintf("./renders/%d_%s.avi", artId, shaderName)
     cmdShady := fmt.Sprintf(
         "%s -i %s -ofmt %s -g %s -f 30",
         shadyPath,
@@ -43,7 +44,74 @@ func Render() (error) {
     cmdCombined := fmt.Sprintf("%s | %s", cmdShady, cmdffmpeg)
     fmt.Println("cmd:" + cmdCombined)
     out, err := exec.Command("bash","-c", cmdCombined).CombinedOutput()
-    fmt.Printf("shady+ffmpeg output: %s\n",out)
+    fmt.Printf(">shady+ffmpeg\n  %s\n",out)
 
 	return err
 }
+// https://medium.com/@j.d.livni/write-a-go-worker-pool-in-15-minutes-c9b42f640923
+type Work struct {
+	ID	int
+	Job models.Artwork//Replace with data needed for rendering
+}
+
+type Worker struct {
+	ID int
+	WorkerChannel chan chan Work // used to communicate between dispatcher and workers
+	Channel chan Work
+	End chan bool
+}
+
+func (w *Worker) Start() {
+	go func() {
+		for {
+			//Provide worker's channel in a queue to notify that it's free
+			w.WorkerChannel <-w.Channel
+			select {
+			case job := <-w.Channel: // worker has received job
+				// work.DoWork(job.Job, w.ID) // do work
+				fmt.Printf("worker %d did some work with artwork %d", w.ID, job.ID)
+			case <-w.End:
+				return
+			}
+		}
+	}()
+}
+
+// end worker
+func (w *Worker) Stop() {
+	fmt.Printf("worker [%d] is stopping", w.ID)
+	w.End <- true
+}
+
+//TODO: Rewrite cmd calls to that
+    // path, err := exec.LookPath("shady")
+    // if err != nil {
+    //    app.serverError(w, errors.New("shady not found"))
+    //    return
+    // }
+    // app.infoLog.Printf("shady path: %s",path)
+    //
+    // shaderName := "seascape"
+    // shaderPath := "./shaders/" + shaderName + ".glsl"
+    // encoding := "x11"
+    // resolution := "1080x1080"
+    // cmdShady := exec.Command("/home/teeth/go/bin/shady", "-i", shaderPath, "-ofmt", encoding, "-g",resolution, "-framerate", "30")
+    // stdoutStderr, err := cmdShady.CombinedOutput()
+
+    // app.infoLog.Printf("shady output: %s",stdoutStderr)
+    // if err != nil {
+    //    app.serverError(w, err)
+    //    return
+    // }
+
+    // videoDuration := "10"
+    // outputPath := "./renders/" + shaderName
+    // cmdffmpeg := exec.Command(
+    //     "/usr/bin/ffmpeg",
+    //     "-f", "rawvideo",
+    //     "-pixel_format", "rgb24",
+    //     "-video_size", "1080x1080",
+    //     "-framerate", "30",
+    //     "-t", videoDuration,
+    //     "-i", "-", outputPath,
+    //     "-y")
